3e9314be63c443aec00f262dd47355b5a13641fa
antonionicolau <antonio.tioypedro1234@gmail.com>
2023-04-01 20:36:25 +0100

    added http package

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:client_ao/main.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
